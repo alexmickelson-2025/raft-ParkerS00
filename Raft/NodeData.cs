1424ac3e6006602ce609ad7695a36e211b64a93f
ParkerS00 <parkerswenson00@gmail.com>
2025-02-03 11:24:44 -0700

    Figure it out!

---
+++ Raft/NodeData.cs
@@ -0,0 +1,11 @@
+﻿namespace ClassLibrary;
+
+public record NodeData
+{
+    public int Id { get; set; } 
+    public int LeaderId { get; set; }
+    public int Term { get; set; }
+    public int CommitIndex { get; set; }
+    public List<Log>? Logs { get; set; }
+    public State State { get; set; }
+}
--- dev.Dockerfile
+++ dev.Dockerfile
@@ -1,9 +1,8 @@
-FROM mcr.microsoft.com/dotnet/sdk:8.0
+FROM mcr.microsoft.com/dotnet/sdk:9.0
  
 RUN apt-get update && \
     apt-get install -y curl && \
     rm -rf /var/lib/apt/lists/*
- 
  
 RUN groupadd -g 1000 developer && \
     useradd -r -u 1000 -g developer -m -s /usr/bin/bash developer && \
